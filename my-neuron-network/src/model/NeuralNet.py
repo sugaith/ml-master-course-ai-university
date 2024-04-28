832bb568faefe208dc0df8f649cb21272a9c74fb
sugaith <sugaith@gmail.com>
2024-04-27 21:18:46 -0300

    wip: back_propagation 90%

--- my-neuron-network/src/model/NeuralNet.py
+++ my-neuron-network/src/model/NeuralNet.py
@@ -1,8 +1,12 @@
 import numpy as np
-from src.model.activation_functions import sigmoid
+from src.model.activation_functions import sigmoid, sigmoid_gradient
+
 
 class NeuralNet:
-    def __init__(self, input_count: int, hidden_count: int, output_count: int, activation: callable(np.float32)):
+    def __init__(self,
+                 input_count: int, hidden_count: int, output_count: int,
+                 activation: callable(np.float32),
+                 learning_rate: np.float32 = 0.1):
         self.input_count = input_count
         self.hidden_count = hidden_count
         self.output_count = output_count
@@ -20,25 +24,68 @@ class NeuralNet:
         print('self.biases_output\n', self.biases_output)
 
         self.activation = activation
+        self.learning_rate = learning_rate
 
     def feed_forward(self, inputs: np.ndarray) -> np.ndarray:
-        inputs2hidden = np.matmul(inputs, self.weights_inputs2hidden)
+        inputs2hidden: np.ndarray = np.matmul(inputs, self.weights_inputs2hidden)
         inputs2hidden = np.add(inputs2hidden, self.biases_hidden)
-        print('inputs2hidden')
-        print(inputs2hidden)
         inputs2hidden = np.vectorize(self.activation)(inputs2hidden)
-        print('inputs2hidden vectorize')
-        print(inputs2hidden)
 
-        hidden2output = np.matmul(inputs2hidden, self.weights_hidden2output)
+        hidden2output: np.ndarray = np.matmul(inputs2hidden, self.weights_hidden2output)
         hidden2output = np.add(hidden2output, self.biases_output)
         hidden2output = np.vectorize(self.activation)(hidden2output)
 
         return hidden2output
 
+    def back_propagation(self, inputs: np.ndarray, targets: np.ndarray):
+        # # Apply feed_forward
+        inputs2hidden: np.ndarray = np.matmul(inputs, self.weights_inputs2hidden)
+        inputs2hidden = np.add(inputs2hidden, self.biases_hidden)
+        inputs2hidden = np.vectorize(self.activation)(inputs2hidden)
 
+        hidden2output: np.ndarray = np.matmul(inputs2hidden, self.weights_hidden2output)
+        hidden2output = np.add(hidden2output, self.biases_output)
+        outputs = np.vectorize(self.activation)(hidden2output)
+        # outputs = self.feed_forward(inputs)
+        print('outputs\n', outputs)
 
+        # # Calculate errors (targets - outputs)
+        output_errors = np.subtract(targets, outputs)
+        print('ERROR: ')
+        print(output_errors)
 
+        # # Calculate error gradient:
+        # apply sigmoid derivative (gradient) to outputs
+        # multiply output gradient by the error
+        # multiply again by the learning rate
+        output_gradients = np.vectorize(sigmoid_gradient)(outputs)
+        output_error_gradients = np.matmul(output_gradients, output_errors)
+        output_error_gradients_over_learning_rate = np.matmul(output_error_gradients, self.learning_rate)
+
+        # # Calculate hidden2output Deltas
+        transposed_hidden = np.transpose(inputs2hidden)
+        weights_hidden2output_delta = np.matmul(output_error_gradients_over_learning_rate, transposed_hidden)
+
+        # # Update `weights_hidden2output` with the calculated Deltas (using addition)
+        # todo: is this correct??????
+        self.weights_hidden2output += weights_hidden2output_delta
+
+        # # Calculate the Hidden-layer Error
+        weights_hidden2output_transposed = np.transpose(self.weights_hidden2output)
+        hidden_errors = np.matmul(weights_hidden2output_transposed, output_errors)
+
+        # # Calculate the gradient for Hidden Layer
+        hidden_gradients = np.vectorize(sigmoid_gradient)(inputs2hidden)
+        hidden_error_gradients = np.matmul(hidden_gradients, hidden_errors)
+        hidden_gradients_over_learning_rate = np.matmul(hidden_error_gradients, self.learning_rate)
+
+        # # Calculate input2hidden Deltas
+        transposed_inputs = np.transpose(inputs)
+        weights_inputs2hidden_delta = np.matmul(hidden_gradients_over_learning_rate, transposed_inputs)
+
+        # # Update `weights_inputs2hidden` with the calculated Deltas (addition)
+        # todo: is this correct???
+        self.weights_inputs2hidden += weights_inputs2hidden_delta
 
 
 
@@ -48,7 +95,7 @@ class NeuralNet:
 
 
 if __name__ == '__main__':
-    nn = NeuralNet(2, 2, 1, activation=sigmoid)
-    output = nn.feed_forward(np.array([1, 0]))
-    print('output')
-    print(output)
+    nn = NeuralNet(2, 2, 2, activation=sigmoid)
+    output = nn.back_propagation(np.array([1, 0]), np.array([0.5, 0.5]))
+    # print('output')
+    # print(output)
--- my-neuron-network/src/model/activation_functions.py
+++ my-neuron-network/src/model/activation_functions.py
@@ -6,6 +6,10 @@ def sigmoid(new_input: np.float32) -> np.float32:
     return 1 / (1 + np.exp(-new_input))
 
 
+def sigmoid_gradient(sigmoid_input: np.float32) -> np.float32:
+    return sigmoid_input * (np.float32(1) - sigmoid_input)
+
+
 def hyperbolic_tangent(new_input: np.float32) -> np.float32:
     # normalizes the input returning a float between -1 and 1
     return np.tanh([new_input])[0]
